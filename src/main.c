73eb3fb0831a51dcecbe524c5177ebe90b9a056c
fib4 <90240770+fib4@users.noreply.github.com>
2025-02-11 19:10:43 +0200

    update the main loop

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <SDL2/SDL.h>
+#include <time.h>
 
 #include "chip8.h"
 #include "graphics.h"
@@ -95,6 +96,9 @@ void inputHandler(struct chip8 *chip8){
 
 int main(int argc, char *argv[]){
 
+    clock_t frame_start = 0;
+    clock_t frame_end = 0;
+    double delta, accumulator;
 
     //initialize sdl_window
     struct graphics graphics = {0};
@@ -119,20 +123,34 @@ int main(int argc, char *argv[]){
 
     chip8.state = RUNNING;
 
+    /*
+    * CHIP-8 is not a physical system but an interpreted language, meaning it has
+    * no inherent clock speed. Execution speed historically depended on the
+    * hardware running the interpreter (e.g. COSMAC VIP vs. modern computers).
+    * 
+    * In a proper implementation users should be able to configure
+    * instructions per frame count to achieve the best performance for different programs
+    * 
+    * Currently this implementation defines the instructions per frame value in the source
+    * code as a temporary solution until user-configurable timing is implemented
+    */
+
     while(chip8.state != STOPPED){
 
-        if(chip8.state == CLEAR){
-            graphics_clear_screen(&graphics);
-        }
-
-        if(chip8.state == DRAW){
-            graphics_draw_graphics(&graphics, &chip8);
-        }
-
-        chip8_execute(&chip8);
         inputHandler(&chip8);
-        
-        SDL_Delay(16); //delay for ~60hz/60fps
+
+        delta = (double)(frame_end - frame_start) / CLOCKS_PER_SEC;
+        frame_start = clock();
+        accumulator += delta;
+        while(accumulator >= 0.0167){
+            //execute 11 instructions per frame
+            for(uint8_t i = 0; i < 11; ++i){
+                chip8_execute(&chip8);
+            }
+            graphics_draw_graphics(&graphics, &chip8);
+            accumulator -= 0.0167;
+        }
+        frame_end = clock();
     }
 
     printf("terve vaan\n");
